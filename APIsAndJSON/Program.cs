7917350e8e72d459155a801e8de9b4797591ddeb
Alen Valdez <alenkvaldeztch@gmail.com>
2023-03-06 10:37:34 -0800

    completed

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
         }
     }
 }
